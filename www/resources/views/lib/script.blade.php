c21e287877a33d5f5689e9136c15e925330364ec
junaet_hossain <junaetcse@gmal.com>
2018-12-29 11:35:52 +0600

    Changes layout template

---
+++ www/resources/views/lib/script.blade.php
@@ -0,0 +1,21 @@
+<!-- js placed at the end of the document so the pages load faster -->
+<script src="{{ URL::asset('assets/js/jquery.js')}}"></script>
+<script src="{{ URL::asset('assets/js/jquery-1.8.3.min.js')}}"></script>
+<script src="{{ URL::asset('assets/js/bootstrap.min.js')}}"></script>
+<script class="include" type="text/javascript" src="{{ URL::asset('assets/js/jquery.dcjqaccordion.2.7.js')}}"></script>
+<script src="{{ URL::asset('assets/js/jquery.scrollTo.min.js')}}"></script>
+<script src="{{ URL::asset('assets/js/jquery.nicescroll.js')}}" type="text/javascript"></script>
+<script src="{{ URL::asset('assets/js/jquery.sparkline.js')}}"></script>
+
+<!--common script for all pages-->
+<script src="{{ URL::asset('assets/js/common-scripts.js')}}"></script>
+
+<script type="text/javascript" src="{{ URL::asset('assets/js/gritter/js/jquery.gritter.js')}}"></script>
+<script type="text/javascript" src="{{ URL::asset('assets/js/gritter-conf.js')}}"></script>
+
+<!--script for this page-->
+<script src="{{ URL::asset('assets/js/sparkline-chart.js')}}"></script>
+<script src="{{ URL::asset('assets/js/zabuto_calendar.js')}}"></script>
+
+<link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote.css" rel="stylesheet">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote.js"></script>
